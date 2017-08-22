f65e2fec7b306cdf495f725462845d82c1294109
James Kim <james@firstbuild.com>
2017-08-22 12:24:07 -0400

    feat: Create Book component and fetch data after App.js mounts

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 import Bookshelf from './Bookshelf'
+import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
 
+  static propTypes = {
+    bookArray: PropTypes.array.isRequired
+  }
+
   render() {
+
+    const { bookArray } = this.props
+
+    let currentlyReadingBooks =  bookArray.filter((book) => book.shelf === "currentlyReading")
+    let wantToReadBooks =  bookArray.filter((book) => book.shelf === "wantToRead")
+    let readBooks =  bookArray.filter((book) => book.shelf === "read")
+
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -11,9 +23,9 @@ class ListBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <Bookshelf shelfTitle="Currently Reading" />
-            <Bookshelf shelfTitle="Want to Read" />
-            <Bookshelf shelfTitle="Read" />
+            <Bookshelf shelfTitle="Currently Reading" books={currentlyReadingBooks} />
+            <Bookshelf shelfTitle="Want to Read" books={wantToReadBooks} />
+            <Bookshelf shelfTitle="Read" books={readBooks} />
           </div>
         </div>
         <div className="open-search">
