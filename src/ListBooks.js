d80bea9f7c6f4bd7dbfcd0c43e5a8a57f31bdbb3
James Kim <james@firstbuild.com>
2017-08-24 11:38:47 -0400

    refactor: Convert ListBooks.js to stateless component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,52 +3,53 @@ import Bookshelf from "./Bookshelf";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-class ListBooks extends Component {
-  static propTypes = {
-    bookArray: PropTypes.array.isRequired
-  };
+const ListBooks = (props) => {
+  const { bookArray, updateBookInServer } = props;
 
-  render() {
-    const { bookArray, updateBookInServer } = this.props;
+  let currentlyReadingBooks = bookArray.filter(
+    book => book.shelf === "currentlyReading"
+  );
 
-    let currentlyReadingBooks = bookArray.filter(
-      book => book.shelf === "currentlyReading"
-    );
-    let wantToReadBooks = bookArray.filter(book => book.shelf === "wantToRead");
-    let readBooks = bookArray.filter(book => book.shelf === "read");
+  let wantToReadBooks = bookArray.filter(book => book.shelf === "wantToRead");
 
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Bookshelf
-              shelfTitle="Currently Reading"
-              books={currentlyReadingBooks}
-              updateBookRequest={updateBookInServer}
-            />
-            <Bookshelf
-              shelfTitle="Want to Read"
-              books={wantToReadBooks}
-              updateBookRequest={updateBookInServer}
-            />
-            <Bookshelf
-              shelfTitle="Read"
-              books={readBooks}
-              updateBookRequest={updateBookInServer}
-            />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search" className="search-link">
-            Add a book
-          </Link>
+  let readBooks = bookArray.filter(book => book.shelf === "read");
+
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Bookshelf
+            shelfTitle="Currently Reading"
+            books={currentlyReadingBooks}
+            updateBookRequest={updateBookInServer}
+          />
+          <Bookshelf
+            shelfTitle="Want to Read"
+            books={wantToReadBooks}
+            updateBookRequest={updateBookInServer}
+          />
+          <Bookshelf
+            shelfTitle="Read"
+            books={readBooks}
+            updateBookRequest={updateBookInServer}
+          />
         </div>
       </div>
-    );
-  }
-}
+      <div className="open-search">
+        <Link to="/search" className="search-link">
+          Add a book
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+ListBooks.propTypes = {
+  bookArray: PropTypes.array.isRequired,
+  updateBookInServer: PropTypes.func.isRequired
+};
 
 export default ListBooks;
