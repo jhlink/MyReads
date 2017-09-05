93ce5d8bb7cafba786d437f58247ab2994f14ae5
James Kim <james@firstbuild.com>
2017-09-05 13:36:36 -0400

    refactor: Remove Component library in ListBooks and Books

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Bookshelf from "./Bookshelf";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
